954421d4f35bd6b9ac91e89af13054425ddbcf49
Saad Saleem <saad.baffa@gmail.com>
2025-09-25 14:13:50 +0500

    play sound using rodio

--- rust/play_sound/src/main.rs
+++ rust/play_sound/src/main.rs
@@ -4,7 +4,7 @@ use std::{fs::File, io::BufReader};
 fn main() {
     let stream_handler = OutputStreamBuilder::open_default_stream().expect("Failed to load the audio stream");
     let sink = Sink::connect_new(&stream_handler.mixer());
-    
+
     let file = File::open("sound.mp3").unwrap();
     let source = Decoder::new(BufReader::new(file)).unwrap();
 
