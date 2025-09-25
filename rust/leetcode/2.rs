96e5e15ccae0ff1e518c631fd9cd88c1b1290946
Saad Saleem <saad.baffa@gmail.com>
2025-09-25 13:49:40 +0500

    play sound using rodio crate

--- rust/leetcode/2.rs
+++ rust/leetcode/2.rs
@@ -1,6 +1,6 @@
 /*
     Problem: Palindrome Number
-    
+
     Description:
     Given an integer x, return true if x is a palindrome, and false otherwise.
 
@@ -8,17 +8,18 @@
     Input: x = 121
     Output: true
     Explanation: 121 reads as 121 from left to right and from right to left.
-    
+
     Example 2:
     Input: x = -121
     Output: false
     Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-    
+
     Example 3:
     Input: x = 10
     Output: false
     Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 */
+
 fn main() {
     let nums: Vec<i32> = vec![121, -121, 10, 12321];
 
@@ -35,10 +36,10 @@ fn is_palindrome(mut x: i32) -> bool {
     if x < 10 {
         return true;
     }
-    
+
     let original = x;
     let mut reversed = 0;
-    
+
     while x > 0 {
         reversed = reversed * 10 + x % 10;
         x /= 10;
---
+++ rust/play_sound/src/main.rs
@@ -0,0 +1,13 @@
+use rodio::{Decoder, OutputStreamBuilder, Sink};
+use std::{fs::File, io::BufReader};
+
+fn main() {
+    let stream_handler = OutputStreamBuilder::open_default_stream().expect("Failed to load the audio stream");
+    let sink = Sink::connect_new(&stream_handler.mixer());
+    
+    let file = File::open("sound.mp3").unwrap();
+    let source = Decoder::new(BufReader::new(file)).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
+}
