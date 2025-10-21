ef60e3ab5f412575c2bfed7cb9dddaf5558ff17e
Saad Saleem <saad.baffa@gmail.com>
2025-10-21 14:54:05 +0500

    added pagination

--- rust/web_scraper/src/main.rs
+++ rust/web_scraper/src/main.rs
@@ -11,47 +11,66 @@ struct Quote {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Target website
-    let url = "https://quotes.toscrape.com/";
-
-    println!("\nFetching data from the: {}", url);
-
-    // Fetch the HTML page
-    let response = reqwest::get(url).await?.text().await?;
-
-    // Parse HTML
-    let document = Html::parse_document(&response);
-
-    // CSS selectors for quote, text and author
-    let quote_selector = Selector::parse(".quote").unwrap();
-    let text_selector = Selector::parse(".text").unwrap();
-    let author_selector = Selector::parse(".author").unwrap();
-    
-    // Collect scraped data
+    let mut page = 1;
+    let base_url = "https://quotes.toscrape.com/page/{}/";
     let mut quotes: Vec<Quote> = Vec::new();
 
-    for quote in document.select(&quote_selector) {
-        let text = quote
-            .select(&text_selector)
-            .next()
-            .map(|t| t.text().collect::<Vec<_>>().join(""))
-            .unwrap_or_default();
+    loop {
+        let url = base_url.replace("{}", &page.to_string());
+        println!("Fetching page {}: {}", page, url);
+
+        let response = reqwest::get(url).await?;
+
+        if !response.status().is_success() {
+            println!("No more pages found (HTTP({}))", response.status());
+            break;
+        }
+       
+        let html = response.text().await?;
+        let document = Html::parse_document(&html);
+
+        // Selectors
+        let quote_selector = Selector::parse(".quote").unwrap();
+        let text_selector = Selector::parse(".text").unwrap();
+        let author_selector = Selector::parse(".author").unwrap();
+        let next_selector = Selector::parse(".next > a").unwrap();
+
+        let mut found_quotes = 0;
+
+        for quote in document.select(&quote_selector) {
+            let text = quote
+                .select(&text_selector)
+                .next()
+                .map(|t| t.text().collect::<Vec<_>>().join(""))
+                .unwrap_or_default();
+            
+            let author = quote
+                .select(&author_selector)
+                .next()
+                .map(|t| t.text().collect::<Vec<_>>().join(""))
+                .unwrap_or_default();
+       
+            quotes.push(Quote { text, author });
+            found_quotes += 1;
+        }
+
+        println!("\nScraped {} quotes on page {}", found_quotes, page);
         
-        let author = quote
-            .select(&author_selector)
-            .next()
-            .map(|t| t.text().collect::<Vec<_>>().join(""))
-            .unwrap_or_default();
-   
-        quotes.push(Quote { text, author });
+        // Check if the next link exists
+        let has_next = document.select(&next_selector).next().is_some();
+    
+        if has_next {
+            page += 1;
+        } else {
+            println!("\nNo more quotes to scrape.");
+            break;
+        } 
     }
+   
+    println!("\nTotal quotes scraped {}.", quotes.len());
 
-    println!("\nScraped {} quotes.", quotes.len());
-
-    // Save to JSON
+    // Save results
     save_to_json(&quotes)?;
-
-    // Save to CSV
     save_to_csv(&quotes)?;
 
     println!("\nData saved to quotes.json and quotes.csv\n");
