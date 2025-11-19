f7999ca8fdde5fa813cf2e10db7ab8c5de196042
Saad Saleem <saad.baffa@gmail.com>
2025-11-19 23:39:31 +0500

    added SQLite

--- rust/todo-app/src/main.rs
+++ rust/todo-app/src/main.rs
@@ -1,4 +1,10 @@
-use std::{io::{self, Write}, str::FromStr};
+use std::{
+    io::{self, Write},
+    process::Command,
+    str::FromStr,
+};
+
+use rusqlite::{params, Connection, Result};
 
 struct Task {
     id: u32,
@@ -7,14 +13,6 @@ struct Task {
 }
 
 impl Task {
-    fn new(id: u32, description: String) -> Self {
-        Self {
-            id,
-            description,
-            completed: false,
-        }
-    }
-
     fn display(&self) {
         let status = if self.completed { "Yes" } else { "No" };
         println!("{:<5}{:<10}{:<50}", self.id, status, self.description);
@@ -22,63 +20,108 @@ impl Task {
 }
 
 struct TodoApp {
-    tasks: Vec<Task>,
-    next_id: u32,
+    conn: Connection,
 }
 
 impl TodoApp {
-    fn new() -> Self {
-        Self {
-            tasks: Vec::new(),
-            next_id: 1,
-        }
+    fn new() -> Result<Self> {
+        let conn = Connection::open("db.sqlite3")?;
+
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                description TEXT NOT NULL,
+                completed INTEGER NOT NULL
+            )",
+            [],
+        )?;
+
+        Ok(Self { conn })
     }
 
-    fn add_task(&mut self) {
+    fn add_task(&self) {
         let description: String = get_input("\nEnter task description: ");
-        let task = Task::new(self.next_id, description);
-        
-        self.tasks.push(task);
-        
+
+        self.conn
+            .execute(
+                "INSERT INTO tasks (description, completed) VALUES (?1, 0)",
+                params![description],
+            )
+            .unwrap();
+
         println!("\nTask added succesfully.");
-        
-        self.next_id += 1;
     }
 
     fn view_tasks(&self) {
-        if self.tasks.is_empty() {
-            println!("\nNo tasks yet.");
-        } else {
-            println!("\n{:<5}{:<10}{:<50}", "ID", "STATUS", "DESCRIPTION");
-            for task in &self.tasks {
-                task.display();
+        let mut stmt = self
+            .conn
+            .prepare("SELECT id, description, completed FROM tasks ORDER BY id")
+            .unwrap();
+
+        let tasks = stmt
+            .query_map([], |row| {
+                Ok(Task {
+                    id: row.get(0)?,
+                    description: row.get(1)?,
+                    completed: row.get::<_, u32>(2)? == 1,
+                })
+            })
+            .unwrap();
+
+        let mut tasks_found: bool = false;
+
+        for task in tasks {
+            if !tasks_found {
+                println!("\n{:<5}{:<10}{:<50}", "ID", "STATUS", "DESCRIPTION");
+                tasks_found = true;
             }
+
+            task.unwrap().display();
+        }
+
+        if !tasks_found {
+            println!("\nNO TASKS YET.");
         }
     }
 
-    fn update_task(&mut self) {
+    fn update_task(&self) {
         let id: u32 = get_input("\nEnter task ID (complete/incomplete): ");
-        if let Some(task) = self.tasks.iter_mut().find(|t| t.id == id) {
-            task.completed = !task.completed;
-            println!("\nTask {} updated", id);
-        } else {
+
+        let rows_affected = self
+            .conn
+            .execute(
+                "UPDATE tasks SET completed = NOT completed WHERE id = ?1",
+                params![id],
+            )
+            .unwrap();
+
+        if rows_affected == 0 {
             println!("\nTask not found.")
+        } else {
+            println!("\nTask {} updated.", id);
         }
     }
 
-    fn delete_task(&mut self) {
+    fn delete_task(&self) {
         let id: u32 = get_input("\nEnter task id to delete: ");
-        if let Some(pos) = self.tasks.iter().position(|t| t.id == id) {
-            self.tasks.remove(pos);
-            println!("\nTask {} deleted", id);
+
+        let rows_affected = self
+            .conn
+            .execute("DELETE FROM tasks WHERE id = ?1", params![id])
+            .unwrap();
+
+        if rows_affected == 0 {
+            println!("\nTask not found.")
         } else {
-            println!("\nTask not found.");
+            println!("\nTask {} deleted.", id);
         }
     }
 }
 
-fn main() {
-    let mut app = TodoApp::new();
+fn main() -> Result<()> {
+    let app = TodoApp::new()?;
+
+    clear_screen();
 
     loop {
         println!("\n============ Todo-App ============");
@@ -89,7 +132,7 @@ fn main() {
         println!("5 - Exit");
 
         let choice: u32 = get_input("\nEnter your choice: ");
-        
+
         match choice {
             1 => app.add_task(),
             2 => app.view_tasks(),
@@ -98,12 +141,14 @@ fn main() {
             5 => {
                 println!("\nGoodbye!\n");
                 break;
-            },
+            }
             _ => {
                 println!("\nInvalid choice. Please try again.");
             }
         }
     }
+
+    Ok(())
 }
 
 fn get_input<T: FromStr>(prompt: &str) -> T {
@@ -112,11 +157,21 @@ fn get_input<T: FromStr>(prompt: &str) -> T {
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to get input");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to get input");
 
         match input.trim().parse::<T>() {
             Ok(value) => return value,
             Err(_) => println!("Invalid input. Please try again."),
         }
+    }
+}
+
+fn clear_screen() {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd").args(["/C", "cls"]).status().unwrap();
+    } else {
+        Command::new("clear").status().unwrap();
     }
 }
--- rust/todo-app/target/debug/deps/todo_app-74d8da10020ceb78.d
+++
@@ -1,5 +0,0 @@
-/Users/saadsaleem/Projects/youtube-coding/rust/todo-app/target/debug/deps/libtodo_app-74d8da10020ceb78.rmeta: src/main.rs
-
-/Users/saadsaleem/Projects/youtube-coding/rust/todo-app/target/debug/deps/todo_app-74d8da10020ceb78.d: src/main.rs
-
-src/main.rs:
