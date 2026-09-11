202aa914e2fc795e1c8dca6defbbf4a6e8e5d8e2
Saad Saleem <saad.baffa@gmail.com>
2026-09-11 11:31:21 +0500

    reverted main file

--- rust/egui_notes_app/src/main.rs
+++ rust/egui_notes_app/src/main.rs
@@ -1,13 +1,3 @@
-use eframe::egui;
-use egui_notes_app::{app::NotesApp, db::Database};
-
-fn main() -> eframe::Result<()> {
-    let database = Database::new().expect("Failed to initialize the database");
-
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([1200.0, 750.0])
-            .with_min_inner_size([900.0, 600.0]),
-        ..Default::default()
-    };
+fn main() {
+    println!("Hello, World!");
 }
